6edabd8ec661d3d48d4d06ac686bd79dc08b177b
João Marcos <jmarcos_duarte@hotmail.com>
2021-11-24 23:56:41 -0400

    Primeiros Passos

---
+++ OlaMundo/src/olamundo/OlaMundo.java
@@ -0,0 +1,11 @@
+package olamundo;
+
+public class OlaMundo {
+	public static void main(String[] args) {
+		
+		System.out.println("Olá, Mundo do Desenvolvimento Java!!!\\o/\\o/\\o/");
+		System.out.println("KA-ME HA-ME HÁ-HÁ-HÁ-HÁ-HÁ-HÁ-HÁ-HÁ==O))))");
+		
+	}
+
+}
