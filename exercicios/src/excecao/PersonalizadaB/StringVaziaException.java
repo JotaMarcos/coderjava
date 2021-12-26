45c281c25cab85c79a8695542069fbb4761661e1
João Marcos Aires Duarte <jmarcos_duarte@hotmail.com>
2021-12-25 21:36:02 -0400

    Tratamento de Exceção

---
+++ exercicios/src/excecao/PersonalizadaB/StringVaziaException.java
@@ -0,0 +1,17 @@
+package excecao.PersonalizadaB;
+
+@SuppressWarnings("serial")
+public class StringVaziaException extends Exception {
+	
+	private String nomeDoAtributo;
+	public StringVaziaException(String nomeDoAtributo) {
+		this.nomeDoAtributo = nomeDoAtributo;
+	}
+	
+	public String getMessage() {
+		return String.format("O atributo '%s' está vazio", 
+				nomeDoAtributo);
+	}
+	
+	
+}
